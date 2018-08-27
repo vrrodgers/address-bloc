979a198fcfca5cbb80aca9859cef0e1439c85f1c
vrrodgers <trulymissvicky1982@gmail.com>
2018-08-27 16:33:42 -0500

    greeting program

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,2 @@
 puts "Welcome to AddressBloc!"
+
---
+++ greeting.rb
@@ -0,0 +1,12 @@
+def hello_world
+
+   ARGV.each do |arg|
+     puts " Hey #{arg}!"
+   end
+   
+   ARGV.each do |arg|
+     puts " Yo #{arg2}!"
+   end
+end
+
+hello_world
